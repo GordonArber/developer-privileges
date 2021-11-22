2c2f2fecb9145614b6072ff46b17e543b6794a2a
Gordon Arber <Gordon.arber@gmail.com>
2021-11-22 10:30:12 -0500

    Initial commit

---
+++ DeveloperPrivilegesTests.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using Exercism.Tests;
+
+public class DeveloperPrivilegesTests
+{
+    [Fact]
+    [Task(1)]
+    public void GetAdmin()
+    {
+        var authenticator = new Authenticator();
+        var admin = authenticator.Admin;
+        string[] actual =
+        {
+            admin?.Email,
+            admin?.FacialFeatures.EyeColor,
+            admin?.FacialFeatures.PhiltrumWidth.ToString(),
+            admin?.NameAndAddress[0]
+        };
+        string[] expected =
+        {
+            "admin@ex.ism",
+            "green",
+            "0.9",
+            "Chanakya"
+        };
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    [Task(2)]
+    public void GetDevelopers()
+    {
+        var authenticator = new Authenticator();
+        var developers = authenticator.Developers;
+        string[] actual =
+        {
+            developers["Bertrand"]?.Email,
+            developers["Bertrand"]?.FacialFeatures.EyeColor,
+            developers["Anders"]?.FacialFeatures.PhiltrumWidth.ToString(),
+            developers["Anders"]?.NameAndAddress[1]
+        };
+        string[] expected =
+        {
+            "bert@ex.ism",
+            "blue",
+            "0.85",
+            "Redmond"
+        };
+        Assert.Equal(expected, actual);
+    }
+}
